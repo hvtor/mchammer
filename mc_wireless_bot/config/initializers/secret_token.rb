3e08a4895fd6f8d1b52da64e6a37898d11978334
Hemant V. Torsekar <your_email@example.com>
2013-04-08 10:21:46 -0400

    Created wireless_bot server for deployment to mobile device.

---
+++ mc_wireless_bot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McWirelessBot::Application.config.secret_token = 'f640311971fe68562187a489d5068b02fcba16b1852177ab22cdc1eea07962d55e39adfabc466779323cf27dd828147500cb36662c3467f79ed2f06d5b742e80'
