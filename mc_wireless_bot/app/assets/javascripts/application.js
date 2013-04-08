d381f56f2a57dec94309a381a78d0d7b44070a62
Hemant V. Torsekar <your_email@example.com>
2013-04-08 12:58:32 -0400

    Creating orientation changer. a b g. Working gyro.

--- mc_wireless_bot/app/assets/javascripts/application.js
+++ mc_wireless_bot/app/assets/javascripts/application.js
@@ -18,11 +18,51 @@
 
 $(function() {
 
-$(window).on("onorientationchange", function(event){
-	
+console.log("Test1");
+window.ondeviceorientation = function(event){
+
+	console.log("Test2");
 	var a = Math.round(event.alpha*1/1);
 	var b = Math.round(event.beta*1/1);
 	var g = Math.round(event.gamma*1/1);
 	console.log(a);
-})
-});
+	console.log(b);
+	console.log(g);
+};
+
+window.ondeviceorientation();
+
+
+// var orientation = window.orientation;
+// switch(orientation) {
+
+
+
+
+// }
+
+
+
+
+// var url = "http://192.168.1.54:8071/motion-control/update";
+
+// function action(selector, _data, message) {
+//   $(selector).on("click", function() {
+// 	console.log(selector)
+//     $.ajax(url, { dataType: "jsonp", data: _data, success: function() {
+//       console.log(message);
+//     }})
+//   })
+// }
+
+//   action(".north", {forward: +1}, "moving forward..");
+//   action(".south", {forward: -1}, "moving backward..");
+//   action(".east", {turn: +1}, "turning right..");
+//   action(".west", {turn: -1}, "turning left..");
+//   action(".strafeRight", {strafe: +1}, "strafing right..");
+//   action(".strafeLeft", {strafe: -1}, "strafing left..");
+//   action(".gamePad", {forward:0}, "stopping..");
+
+// })
+
+// });
